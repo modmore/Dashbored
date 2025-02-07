8a02577547f3c8c7225ddb0a91045469836e4160
Murray Wood <murray@digitalpenguin.studio>
2025-02-07 15:17:36 +0800

    Prevent data for a section being returned from the processor if hidden via system/user setting

--- core/components/dashbored/processors/mgr/sitedash/refresh.class.php
+++ core/components/dashbored/processors/mgr/sitedash/refresh.class.php
@@ -33,10 +33,37 @@ class DashboredSiteDashRefreshProcessor extends DashboredSiteDashAbstractProcess
 
         if ($this->refresh || !$data) {
             $data = $this->getSiteDashData();
+            $data = $this->filterData($data);
             $this->modx->cacheManager->set('sitedash_data', $data, 7200, Dashbored::$cacheOptions);
         }
 
         return $data ?? [];
+    }
+
+    /**
+     * If parts of the sitedash widget are hidden via system/user settings, then remove that data from the output
+     * @param array $data
+     * @return array
+     */
+    protected function filterData(array $data): array
+    {
+        if (!$this->modx->getOption('dashbored.sitedash.display_lighthouse')) {
+            unset($data['lighthouse']);
+        }
+
+        if (!$this->modx->getOption('dashbored.sitedash.display_config')) {
+            unset($data['config']);
+        }
+
+        if (!$this->modx->getOption('dashbored.sitedash.display_security')) {
+            unset($data['security']);
+        }
+
+        if (!$this->modx->getOption('dashbored.sitedash.display_checks')) {
+            unset($data['checks']);
+        }
+
+        return $data;
     }
 }
 return 'DashboredSiteDashRefreshProcessor';
